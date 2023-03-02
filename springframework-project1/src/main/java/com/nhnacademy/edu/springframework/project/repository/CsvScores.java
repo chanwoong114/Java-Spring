1cd2b9173f11bd5d29a925feeda937e9ae8357e5
chanwoong114 <cksdnd10001@gmail.com>
2023-03-03 01:20:18 +0900

    23/03/02-home

--- springframework-project1/src/main/java/com/nhnacademy/edu/springframework/project/repository/CsvScores.java
+++ springframework-project1/src/main/java/com/nhnacademy/edu/springframework/project/repository/CsvScores.java
@@ -6,12 +6,13 @@ import java.util.List;
 public class CsvScores implements Scores {
 
     private CsvScores(){}
+    private static CsvScores csvScores = new CsvScores();
 
     /** TODO 2 :
      * Java Singleton 패턴으로 getInstance() 를 구현하세요.
      **/
     public static Scores getInstance() {
-        return null;
+        return csvScores;
     }
 
     // TODO 5 : score.csv 파일에서 데이터를 읽어 멤버 변수에 추가하는 로직을 구현하세요.
