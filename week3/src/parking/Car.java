181647af061573c9f9f5f8463ed3dd8251466d99
chanwoong114 <cksdnd10001@gmail.com>
2023-03-09 21:57:52 +0900

    03/09-home

---
+++ week3/src/parking/Car.java
@@ -0,0 +1,13 @@
+package parking;
+
+public class Car {
+    private int number;
+
+    public Car(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+}
