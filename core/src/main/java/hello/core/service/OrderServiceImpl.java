af6170ce29e6422e527cfdfe608621e8c19e525f
SSAFY <cksdnd10001@gmail.com>
2023-03-30 10:43:39 +0900

    03/30

--- core/src/main/java/hello/core/service/OrderServiceImpl.java
+++ core/src/main/java/hello/core/service/OrderServiceImpl.java
@@ -6,12 +6,21 @@ import hello.core.domain.Member;
 import hello.core.order.Order;
 import hello.core.repository.MemberRepository;
 import hello.core.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
 
     private final DiscountPolicy discountPolicy;
 
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+
+    // 생성자가 하나라면 Autowired 생량 가능 (스프링 빈에만 해당)
+    @Autowired
     public OrderServiceImpl(DiscountPolicy discountPolicy, MemberRepository memberRepository) {
         this.discountPolicy = discountPolicy;
         this.memberRepository = memberRepository;
