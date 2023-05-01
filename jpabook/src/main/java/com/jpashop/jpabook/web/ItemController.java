333648dc6a73f89ca8d0e0e305a24ef56f6990a6
chanwoong114 <cksdnd10001@gamil.com>
2023-05-01 17:54:41 +0900

    0501

---
+++ jpabook/src/main/java/com/jpashop/jpabook/web/ItemController.java
@@ -0,0 +1,20 @@
+package com.jpashop.jpabook.web;
+
+import com.jpashop.jpabook.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+    private final ItemService itemService;
+
+    @GetMapping(value = "/items/new")
+    public String createForm(Model model) {
+
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+}
